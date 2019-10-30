8d8b47e8901cc5fc41ae68569938005c207e8b2b
thiennd <thiennd@nal.vn>
2019-10-30 10:38:41 +0700

    add library stetho to show database

--- app/src/main/java/beau/com/wall/wallpaperapp/App.java
+++ app/src/main/java/beau/com/wall/wallpaperapp/App.java
@@ -1,4 +1,12 @@
 package beau.com.wall.wallpaperapp;
 
-public class App {
+import android.app.Application;
+
+import com.facebook.stetho.Stetho;
+
+public class App extends Application {
+    public void onCreate() {
+        super.onCreate();
+        Stetho.initializeWithDefaults(this);
+    }
 }
